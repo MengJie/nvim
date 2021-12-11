5fa87f90e99733e0422cc20e8fac70650d2a03c5
Jie Meng <inmouse@gmail.com>
2021-12-11 21:08:13 +0800

    feat: 加入tab键相关的配置

---
+++ lua/opts.lua
@@ -0,0 +1,11 @@
+local o = vim.o
+
+-- tab key
+o.tabstop = 2
+o.softtabstop = -1  -- refer to shiftwidth
+o.shiftwidth = 0    -- refer to tabstop
+
+o.expandtab = true  -- use space instead tab
+
+o.autoindent = true -- reproduce the indentation of the previous line
+
